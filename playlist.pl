cb7d841b0ca7b1bfa2513c73bf552e5d6568d973
zleight1 <zleight1@gmail.com>
2014-10-06 10:24:29 -0400

    Fisher Yates Shuffle Perl Implementation http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle

--- playlist.pl
+++ playlist.pl
@@ -8,10 +8,47 @@ use warnings;
 # Date: 10/6/14
 # Purpose: Playlist Creation/Randomizer
 
-sub randomizePlaylist {
-    my @songs = @_;
-    
+use Data::Dumper;
 
+#this gets a song list in order
+# my @songList = @{generateSongList(50)};
+
+print Dumper(randomizePlaylist(generateSongList(50)));
+
+#generate a song list in order, 0 - n
+sub generateSongList {
+    my $n = shift;
+    my @songList;
+    #0 to n
+    foreach (0..$n) {
+        #push the song onto the array with the #
+        push @songList, "Song #$_";
+    }
+    #return reference for memory reasons
+    return \@songList;
 }
+
+#this randomizes the song
+sub randomizePlaylist {
+    my $songs = shift;
+    die 'NOT ARRAY REF OF SONGS' unless(ref($songs) eq 'ARRAY');
+
+    my @songs = @$songs; #take first argument and derefence to array
+
+    ### fisher yates shuffle
+    for (my $i = scalar(@songs)-1; $i > 0; $i--) {
+        my $j = int(rand($i));
+
+        #we need a temp variable for readability
+        my $temp = $songs[$j];
+
+        #swap indexes at i and j
+        $songs[$j] = $songs[$i];
+        $songs[$i] = $temp;
+    }
+
+    return \@songs;
+}
+
 
 1;
